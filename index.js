f8f8d35335bb0947997227bbae49416f95219c6c
Focus04 <alexogrezeanu2004@gmail.com>
2021-04-20 00:27:09 +0300

    Improved the slider

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const slides = document.querySelectorAll('.slide');
+const circles = document.querySelectorAll('.circle');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 
 const nextSlide = () => {
   const current = document.querySelector('.current');
+  const currentCircle = document.querySelector('.current-circle');
   current.classList.remove('current');
-  if (current.nextElementSibling) current.nextElementSibling.classList.add('current');
-  else slides[0].classList.add('current');
+  currentCircle.classList.remove('current-circle');
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add('current');
+    currentCircle.nextElementSibling.classList.add('current-circle');
+  } else {
+    slides[0].classList.add('current');
+    circles[0].classList.add('current-circle');
+  }
 };
 
 const prevSlide = () => {
   const current = document.querySelector('.current');
+  const currentCircle = document.querySelector('.current-circle');
   current.classList.remove('current');
-  if (current.previousElementSibling) current.previousElementSibling.classList.add('current');
-  else slides[slides.length - 1].classList.add('current');
+  currentCircle.classList.remove('current-circle');
+  if (current.previousElementSibling) {
+    current.previousElementSibling.classList.add('current');
+    currentCircle.previousElementSibling.classList.add('current-circle');
+  } else {
+    slides[slides.length - 1].classList.add('current');
+    circles[slides.length - 1].classList.add('current-circle');
+  }
 };
 
 let slideInterval;
@@ -26,5 +41,17 @@ prev.addEventListener('click', () => {
   prevSlide();
   clearInterval(slideInterval);
   slideInterval = setInterval(nextSlide, 10000);
+});
+circles.forEach((circle) => {
+  circle.addEventListener('click', () => {
+    const current = document.querySelector('.current');
+    const currentCircle = document.querySelector('.current-circle');
+    current.classList.remove('current');
+    currentCircle.classList.remove('current-circle');
+    circles[[...circles].indexOf(circle)].classList.add('current-circle');
+    slides[[...circles].indexOf(circle)].classList.add('current');
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, 10000);
+  });
 });
 slideInterval = setInterval(nextSlide, 10000);
