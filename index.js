17e0eb719000457509233ad58631f8a51a3a63e5
Focus04 <alexogrezeanu2004@gmail.com>
2021-07-09 17:48:47 +0300

    Built a backend tool to query info using gamedig

--- index.js
+++ index.js
@@ -1,68 +1,14 @@
-const slides = document.querySelectorAll('.slide');
-const circles = document.querySelectorAll('.circle');
-const next = document.querySelector('#next');
-const prev = document.querySelector('#prev');
-const show = document.querySelector('#show');
+const express = require('express');
+const gamedig = require('gamedig');
 
-const nextSlide = () => {
-  const current = document.querySelector('.current');
-  const currentCircle = document.querySelector('.current-circle');
-  current.classList.remove('current');
-  currentCircle.classList.remove('current-circle');
-  if (current.nextElementSibling) {
-    current.nextElementSibling.classList.add('current');
-    currentCircle.nextElementSibling.classList.add('current-circle');
-  } else {
-    slides[0].classList.add('current');
-    circles[0].classList.add('current-circle');
-  }
-};
-
-const prevSlide = () => {
-  const current = document.querySelector('.current');
-  const currentCircle = document.querySelector('.current-circle');
-  current.classList.remove('current');
-  currentCircle.classList.remove('current-circle');
-  if (current.previousElementSibling) {
-    current.previousElementSibling.classList.add('current');
-    currentCircle.previousElementSibling.classList.add('current-circle');
-  } else {
-    slides[slides.length - 1].classList.add('current');
-    circles[slides.length - 1].classList.add('current-circle');
-  }
-};
-
-let slideInterval;
-next.addEventListener('click', () => {
-  nextSlide();
-  clearInterval(slideInterval);
-  slideInterval = setInterval(nextSlide, 10000);
-});
-prev.addEventListener('click', () => {
-  prevSlide();
-  clearInterval(slideInterval);
-  slideInterval = setInterval(nextSlide, 10000);
-});
-circles.forEach((circle) => {
-  circle.addEventListener('click', () => {
-    const current = document.querySelector('.current');
-    const currentCircle = document.querySelector('.current-circle');
-    current.classList.remove('current');
-    currentCircle.classList.remove('current-circle');
-    circles[[...circles].indexOf(circle)].classList.add('current-circle');
-    slides[[...circles].indexOf(circle)].classList.add('current');
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, 10000);
+const app = express();
+app.use(express.static('public'));
+app.listen(5000, () => console.log('App running...'));
+app.get('/api', async (req, res) => {
+  const data = await gamedig.query({
+    type: 'samp',
+    host: '89.40.4.118',
+    port: '7777'
   });
-});
-slideInterval = setInterval(nextSlide, 10000);
-show.addEventListener('click', () => {
-  const table = document.querySelector('.table');
-  if (show.innerHTML === 'Show') {
-    table.style.display = 'block';
-    show.innerHTML = 'Hide';
-  } else {
-    table.style.display = 'none';
-    show.innerHTML = 'Show';
-  }
+  res.json(data);
 });
---
+++ public/slider.js
@@ -0,0 +1,87 @@
+const slides = document.querySelectorAll('.slide');
+const circles = document.querySelectorAll('.circle');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const show = document.querySelector('#show');
+
+const nextSlide = () => {
+  const current = document.querySelector('.current');
+  const currentCircle = document.querySelector('.current-circle');
+  current.classList.remove('current');
+  currentCircle.classList.remove('current-circle');
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add('current');
+    currentCircle.nextElementSibling.classList.add('current-circle');
+  } else {
+    slides[0].classList.add('current');
+    circles[0].classList.add('current-circle');
+  }
+};
+
+const prevSlide = () => {
+  const current = document.querySelector('.current');
+  const currentCircle = document.querySelector('.current-circle');
+  current.classList.remove('current');
+  currentCircle.classList.remove('current-circle');
+  if (current.previousElementSibling) {
+    current.previousElementSibling.classList.add('current');
+    currentCircle.previousElementSibling.classList.add('current-circle');
+  } else {
+    slides[slides.length - 1].classList.add('current');
+    circles[slides.length - 1].classList.add('current-circle');
+  }
+};
+
+const query = async () => {
+  const response = await fetch('/api');
+  const data = await response.json();
+  let playerList = '';
+  data.players.forEach((player) => {
+    const row = `
+      <tr>
+        <td>${player.raw.id}</td>
+        <td>${player.name}</td>
+        <td>${player.raw.score}</td>
+        <td>${player.raw.ping}</td>
+      </tr>
+    `;
+    playerList += row;
+  });
+  document.querySelector('.players').innerHTML = playerList;
+};
+
+let slideInterval;
+next.addEventListener('click', () => {
+  nextSlide();
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, 10000);
+});
+prev.addEventListener('click', () => {
+  prevSlide();
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, 10000);
+});
+circles.forEach((circle) => {
+  circle.addEventListener('click', () => {
+    const current = document.querySelector('.current');
+    const currentCircle = document.querySelector('.current-circle');
+    current.classList.remove('current');
+    currentCircle.classList.remove('current-circle');
+    circles[[...circles].indexOf(circle)].classList.add('current-circle');
+    slides[[...circles].indexOf(circle)].classList.add('current');
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, 10000);
+  });
+});
+slideInterval = setInterval(nextSlide, 10000);
+show.addEventListener('click', () => {
+  query();
+  const table = document.querySelector('.online-table');
+  if (show.innerHTML === 'Show') {
+    table.style.display = 'block';
+    show.innerHTML = 'Hide';
+  } else {
+    table.style.display = 'none';
+    show.innerHTML = 'Show';
+  }
+});
