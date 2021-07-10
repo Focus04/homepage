7c2265605d66add43b10f8083d30fd03e0d2f9e3
Focus04 <alexogrezeanu2004@gmail.com>
2021-07-10 17:25:37 +0300

    Error messages now show in a modal

--- public/slider.js
+++ public/slider.js
@@ -3,6 +3,10 @@ const circles = document.querySelectorAll('.circle');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 const show = document.querySelector('#show');
+const closeBtn = document.querySelector('.close-btn');
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
+const modalMessage = document.querySelector('.message');
 
 const nextSlide = () => {
   const current = document.querySelector('.current');
@@ -38,6 +42,20 @@ const query = async () => {
   return data;
 };
 
+const openModal = () => {
+  modal.style.display = 'block';
+  setTimeout(() => {
+    modal.style.opacity = 1;
+    modalContent.style.transform = 'translateY(100px)';
+  }, 10);
+}
+
+const closeModal = () => {
+  modalContent.style.transform = 'translateY(-100px)';
+  modal.style.opacity = 0;
+  setTimeout(() => modal.style.display = 'none', 400);
+}
+
 let slideInterval;
 next.addEventListener('click', () => {
   nextSlide();
@@ -62,25 +80,37 @@ circles.forEach((circle) => {
   });
 });
 slideInterval = setInterval(nextSlide, 10000);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', (e) => {
+  if (e.target == modal) closeModal();
+});
 show.addEventListener('click', async () => {
-  const data = await query();
-  if (data.msg === 'Server is offline...') return alert(data.msg);
-  if (!data.players[0]) return alert('No player connected.');
-  let playerList = '';
-  data.players.forEach((player) => {
-    const row = `
-      <tr>
-        <td>${player.raw.id}</td>
-        <td>${player.name}</td>
-        <td>${player.raw.score}</td>
-        <td>${player.raw.ping}</td>
-      </tr>
-    `;
-    playerList += row;
-  });
-  document.querySelector('.players').innerHTML = playerList;
   const table = document.querySelector('.online-table');
   if (show.innerHTML === 'Show') {
+    const data = await query();
+    if (data.msg === 'Server is offline...') {
+      modalMessage.innerHTML = data.msg;
+      openModal();
+      return setTimeout(closeModal, 10000);
+    }
+    if (!data.players[0]) {
+      modalMessage.innerHTML = 'No players online...';
+      openModal();
+      return setTimeout(closeModal, 10000);
+    }
+    let playerList = '';
+    data.players.forEach((player) => {
+      const row = `
+        <tr>
+          <td>${player.raw.id}</td>
+          <td>${player.name}</td>
+          <td>${player.raw.score}</td>
+          <td>${player.raw.ping}</td>
+        </tr>
+      `;
+      playerList += row;
+    });
+    document.querySelector('.players').innerHTML = playerList;
     table.style.opacity = 1;
     show.innerHTML = 'Hide';
   } else {
